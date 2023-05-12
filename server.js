78dd9dfd290a45afbf15478a0623af77cf731d52
Stef Timmermans <timmerms@oregonstate.edu>
2023-05-11 20:12:41 -0700

    changed `server.js` to use ./routes/[file] format

--- server.js
+++ server.js
@@ -6,7 +6,12 @@ const app = express();
 
 // Import route files
 const accountsRoutes = require('./routes/accounts');
-// ... Import the rest of the route files here...
+const transactionsRoutes = require('./routes/transactions');
+const transactionStatusRoutes = require('./routes/transactionsStatus');
+const customersRoutes = require('./routes/customers');
+const certificatesRoutes = require('./routes/certificates');
+const customerAccountRoutes = require('./routes/customer_account');
+const accountTransactionRoutes = require('./routes/account_transaction');
 
 // Serve static files
 app.use(express.static(path.join(__dirname, 'public')));
@@ -17,7 +22,12 @@ app.set('view engine', 'hbs');
 
 // Use route files
 app.use('/accounts', accountsRoutes);
-// Insert the rest of the route files here...
+app.use('/transactions', transactionsRoutes);
+app.use('/transactionsStatus', transactionStatusRoutes);
+app.use('/customers', customersRoutes);
+app.use('/certificates', certificatesRoutes);
+app.use('/customer_account', customerAccountRoutes);
+app.use('/account_transaction', accountTransactionRoutes);
 
 // Home page route
 app.get('/', (req, res) => {
