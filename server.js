6f5b33852946d72d84824d596f362a4e13a950fe
Stef Timmermans <timmerms@oregonstate.edu>
2023-05-12 13:01:23 -0700

    Update server.js

--- server.js
+++ server.js
@@ -4,4 +4,10 @@ TODO
 
 RESET SERVER.JS TO ONLY WORK IN FRONT END AND REDO NPM INSTALLS
 
+USE /views TO RENDER PAGES
+`home.hbs` is the home page
+/views/tables are partials to be completed by header and footer files
+
+404 FUNCTIONALITY OPTIONAL
+MOVE ALL .HBS INTO ONE FILE IF ABOSLUTELY NECESSARY
 */
